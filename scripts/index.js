0896df99b74fb7aef411bd291ba9abfc13ccb667
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-02-18 14:31:02 +0300

    refactor: измененить код поведения попапа по замечаниям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,37 @@
-// Кнопка редактирования профиля
+/** Элементы страницы, блок profile */
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 let editButton = document.querySelector('.profile__button_type_edit');
-// Форма изменения профиля в попапе
+
+/** Элементы попапа для редактирования профиля */
+let popup = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
-// Кнопка закрытия попапа
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let closePopupButton = document.querySelector('.popup__cancel-button');
 
-// Открыть попап
-function openPopup () {
-  let popup = document.querySelector('.popup');
 
-  // Сбросить значения в попапе до установленных
-  resetPopup ();
-  // Открыть попап
+/** Функция открывает попап, предварительно установив текущие значения */
+function openPopup () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
 }
 
-// Закрыть попап
+/** Функция закрывает попап */
 function closePopup () {
-  let popup = document.querySelector('.popup');
   popup.classList.remove('popup_opened');
 }
 
-// Сбросить значения полей в попапе на сохраненные
-function resetPopup () {
-  let nameInput = document.querySelector('.popup__input_type_name');
-  let jobInput = document.querySelector('.popup__input_type_job');
-
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__job');
-
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-}
-
-// Сохранить значения из попапа в профиль
-function saveProfileInfo () {
-  let nameInput = document.querySelector('.popup__input_type_name');
-  let jobInput = document.querySelector('.popup__input_type_job');
-
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__job');
-
+/** Функция сохраняет введенные данные и закрывает попап */
+function saveProfileInfo (event) {
+  event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-}
-
-// Закрыть попав С сохранрением информации в профиль
-function closePopupWithSave (evt) {
-  evt.preventDefault();
-  saveProfileInfo ();
   closePopup ();
 }
 
-// Закрыть попав БЕЗ сохранрениея информации
-function closePopupWithoutSave () {
-  resetPopup ();
-  closePopup ();
-}
-
-// Обработчики событий кнопок
+/** Обработчики событий для кнопок */
 editButton.addEventListener('click', openPopup);
-popupForm.addEventListener('submit', closePopupWithSave);
-closePopupButton.addEventListener('click', closePopupWithoutSave);
+popupForm.addEventListener('submit', saveProfileInfo);
+closePopupButton.addEventListener('click', closePopup);
