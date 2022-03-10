4bb6249c449dae7b5bfd687f5d465677078913b7
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-03-10 21:11:20 +0300

    fix: замечание ревью, изменить порядок элементов в скрипте

--- scripts/index.js
+++ scripts/index.js
@@ -1,129 +1,3 @@
-
-/**
- *
- * Общие элементы для всех попапов
- *
- */
-
-/** Элементы попапа */
-const popupCloseButtons = document.querySelectorAll('.popup__cancel-button');
-
-/** Функция открывает нужный попап
- *
- * Ожидаемый аргумент:
- * строка с классом нужного попапа, например: '.popup_type_edit-profile'
- */
-function openPopup (popupName) {
-  document.querySelector(popupName).classList.add('popup_opened');
-}
-
-/** Функция закрывает текущий попап */
-function closePopup (event) {
-  const popup = event.target.closest('.popup');
-  popup.classList.remove('popup_opened');
-}
-
-/** Обработчик нажатия одинаковых кнопок */
-popupCloseButtons.forEach( button => button.addEventListener('click', closePopup) );
-
-
-/**
- *
- * Раздел с профилем пользователя
- *
- */
-
-/** Элементы страницы, блок profile */
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-const profileEditButton = document.querySelector('.profile__button_type_edit');
-
-/** Элементы попапа для редактирования профиля */
-const profileEditPopup = document.querySelector('.popup_type_edit-profile');
-const profileEditForm = profileEditPopup.querySelector('.popup__form');
-const profileNameInput = profileEditPopup.querySelector('.popup__input_type_name');
-const profileJobInput = profileEditPopup.querySelector('.popup__input_type_job');
-
-/** Функция сохраняет введенные данные и закрывает попап */
-function saveProfileInfo (event) {
-  event.preventDefault();
-  profileName.textContent = profileNameInput.value;
-  profileJob.textContent = profileJobInput.value;
-  closePopup (event);
-}
-
-/** Обработчики нажатий кнопок */
-profileEditButton.addEventListener('click', function () {
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
-  openPopup('.popup_type_edit-profile');
-});
-profileEditForm.addEventListener('submit', saveProfileInfo);
-
-
-/**
- *
- * Форма с добавлением карточки
- *
- */
-
-/** Элементы страницы */
-const newCardButton = document.querySelector('.profile__button_type_add');
-
-/** Элементы попапа для добавления карточки */
-const newCardPopup = document.querySelector('.popup_type_add-card');
-const newCardForm = newCardPopup.querySelector('.popup__form');
-const newCardTitle = newCardPopup.querySelector('.popup__input_type_title');
-const newCardLink = newCardPopup.querySelector('.popup__input_type_link');
-
-/** Функция сохраняет введенные данные и закрывает попап */
-function saveNewCard (event) {
-  event.preventDefault();
-
-  const card = {};
-  card.name = newCardTitle.value;
-  card.link = newCardLink.value;
-  drawCards(card);
-
-  closePopup(event);
-  newCardTitle.value = '';
-  newCardLink.value = '';
-}
-
-/** Обработчики нажатий кнопок */
-newCardButton.addEventListener('click', function () {
-  openPopup('.popup_type_add-card');
-});
-newCardForm.addEventListener('submit', saveNewCard);
-
-
-/**
- *
- * Попап с увеличенной картинкой
- *
- */
-
-/** Элементы страницы */
-const imagePopup = document.querySelector('.popup_type_image');
-const imagePopupFigure = imagePopup.querySelector('.popup__image');
-const imagePopupCaption = imagePopup.querySelector('.popup__image-caption');
-
-/** Функция открывает попап с увеличенной картинкой */
-function showImagePopup (event) {
-  imagePopupFigure.src = event.target.src;
-  imagePopupFigure.alt = event.target.alt;
-  imagePopupCaption.textContent = event.target.closest('.card').querySelector('.card__title').textContent;
-
-  openPopup('.popup_type_image');
-}
-
-
-/**
- *
- * Раздел с карточками
- *
- * */
-
 /** Исходные карточки для загрузки */
 const initialCards = [
   {
@@ -152,6 +26,79 @@ const initialCards = [
   }
 ];
 
+
+/** Элементы страницы */
+const profileEditButton = document.querySelector('.profile__button_type_edit');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+const newCardButton = document.querySelector('.profile__button_type_add');
+
+const profileEditPopup = document.querySelector('.popup_type_edit-profile');
+const profileEditForm = profileEditPopup.querySelector('.popup__form');
+const profileNameInput = profileEditPopup.querySelector('.popup__input_type_name');
+const profileJobInput = profileEditPopup.querySelector('.popup__input_type_job');
+
+const newCardPopup = document.querySelector('.popup_type_add-card');
+const newCardForm = newCardPopup.querySelector('.popup__form');
+const newCardTitle = newCardPopup.querySelector('.popup__input_type_title');
+const newCardLink = newCardPopup.querySelector('.popup__input_type_link');
+
+const imagePopup = document.querySelector('.popup_type_image');
+const imagePopupFigure = imagePopup.querySelector('.popup__image');
+const imagePopupCaption = imagePopup.querySelector('.popup__image-caption');
+
+const popupCloseButtons = document.querySelectorAll('.popup__cancel-button');
+
+
+// Функции
+
+/** Функция открывает нужный попап
+ *
+ * Ожидаемый аргумент:
+ * строка с классом нужного попапа, например: '.popup_type_edit-profile'
+ */
+function openPopup (popupName) {
+  document.querySelector(popupName).classList.add('popup_opened');
+}
+
+/** Функция закрывает текущий попап */
+function closePopup (event) {
+  const popup = event.target.closest('.popup');
+  popup.classList.remove('popup_opened');
+}
+
+/** Функция сохраняет введенные данные и закрывает попап */
+function saveProfileInfo (event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileJob.textContent = profileJobInput.value;
+  closePopup (event);
+}
+
+/** Функция сохраняет введенные данные и закрывает попап */
+function saveNewCard (event) {
+  event.preventDefault();
+
+  const card = {};
+  card.name = newCardTitle.value;
+  card.link = newCardLink.value;
+  drawCards(card);
+
+  closePopup(event);
+  newCardTitle.value = '';
+  newCardLink.value = '';
+}
+
+/** Функция открывает попап с увеличенной картинкой */
+function showImagePopup (event) {
+  imagePopupFigure.src = event.target.src;
+  imagePopupFigure.alt = event.target.alt;
+  imagePopupCaption.textContent = event.target.closest('.card').querySelector('.card__title').textContent;
+
+  openPopup('.popup_type_image');
+}
+
 /** Функция генерирует карточки и вставляет их на страницу
  *
  * Ожидаемые аргументы на вход:
@@ -168,7 +115,7 @@ const initialCards = [
  * Результат функции:
  * вставляет сгенерированные карточки на страницу в блок .cards в обратном порядке (то есть вставляет в начало блока)
  */
-function drawCards (...cards) {
+ function drawCards (...cards) {
   const cardsElement = document.querySelector('.cards');
   const cardTemplate = document.querySelector('#card').content;
 
@@ -200,6 +147,30 @@ function likeCard (event) {
 function deleteCard (event) {
   event.target.closest('.card').remove();
 }
+
+
+
+
+// Обработчики событий
+profileEditButton.addEventListener('click', function () {
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+  openPopup('.popup_type_edit-profile');
+});
+profileEditForm.addEventListener('submit', saveProfileInfo);
+
+newCardButton.addEventListener('click', function () {
+  openPopup('.popup_type_add-card');
+});
+newCardForm.addEventListener('submit', saveNewCard);
+
+popupCloseButtons.forEach( button => button.addEventListener('click', closePopup) );
+
+
+
+
+
+
 
 /** Отобразить исходные карточки при загрузке страницы */
 drawCards( ...initialCards );
