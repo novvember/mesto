bfba99e8c2848fcfa5c9f168d39fca71b6cc84e5
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-04-27 20:29:05 +0300

    refactor: изменить порядок кода в index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,31 +28,52 @@ import FormValidator from '../components/FormValidator.js';
 import UserInfo from '../components/UserInfo.js';
 import './index.css'; // импорт css-стилей для сборки в Webpack
 
+// Функции
+const formValidators = {};
+
+function validateForms (formClasses) {
+  const formElements = Array.from(document.querySelectorAll(formClasses.formSelector));
+  formElements.forEach(formElement => {
+    const form = new FormValidator(formClasses, formElement);
+    formValidators[formElement.getAttribute('name')] = form;
+    form.enableValidation();
+  });
+}
+
 function renderCard(data) {
   const card = new Card(data, cardTemplateSelector, handleCardClick);
   return card.generateCard();
 }
 
-
-// Инициализация Section, добавление исходных карточек
 function handleCardClick(imageLink, text) {
   imagePopup.open(imageLink, text);
 }
 
+// Инициализация классов
 const cardsSection = new Section({
   items: initialCards,
   renderer: renderCard
 }, cardsSelector);
 
-cardsSection.renderItems();
-
-
-// Инициализация Popup с редактированием информации о пользователе
 const profileEditPopup = new PopupWithForm(profileEditPopupSelector, data => {
   userInfo.setUserInfo(data);
   profileEditPopup.close();
 });
 
+const newCardPopup = new PopupWithForm(newCardPopupSelector, data => {
+  cardsSection.addItem(renderCard(data));
+  newCardPopup.close();
+  formValidators[newCardForm.getAttribute('name')].disableButtonState();
+});
+
+const imagePopup = new PopupWithImage(imagePopupSelector);
+
+const userInfo = new UserInfo({
+  nameElement: profileName,
+  jobElement: profileJob
+});
+
+// Установка слушателей событий
 profileEditPopup.setEventListeners();
 
 profileEditButton.addEventListener('click', function () {
@@ -65,38 +86,16 @@ profileEditButton.addEventListener('click', function () {
   profileEditPopup.open();
 });
 
-
-// Инициализация Popup с добавлением новой карточки
-const newCardPopup = new PopupWithForm(newCardPopupSelector, data => {
-  cardsSection.addItem(renderCard(data));
-  newCardPopup.close();
-  formValidators[newCardForm.getAttribute('name')].disableButtonState();
-});
-
 newCardPopup.setEventListeners();
 
 newCardButton.addEventListener('click', function () {
   newCardPopup.open();
 });
 
-
-// Инициализация Popup с увеличенным изображением
-const imagePopup = new PopupWithImage(imagePopupSelector);
-
 imagePopup.setEventListeners();
 
-
-// Инициализация FormValidator
-const formValidators = {};
-
-function validateForms (formClasses) {
-  const formElements = Array.from(document.querySelectorAll(formClasses.formSelector));
-  formElements.forEach(formElement => {
-    const form = new FormValidator(formClasses, formElement);
-    formValidators[formElement.getAttribute('name')] = form;
-    form.enableValidation();
-  });
-}
+// Вызов функций и методов при загрузке страницы
+cardsSection.renderItems();
 
 validateForms({
   formSelector,
@@ -105,11 +104,4 @@ validateForms({
   inactiveButtonClass,
   inputErrorClass,
   errorClass
-});
-
-
-// Инициализация UserInfo
-const userInfo = new UserInfo({
-  nameElement: profileName,
-  jobElement: profileJob
 });
