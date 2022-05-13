01bf7484eeca17bbcac65e4e77b680640cb91653
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-05-13 14:20:01 +0300

    refactor: добавить комментарии в PopupWithConfirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,12 +1,20 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithConfirmation extends Popup {
+  /**
+   * Отвечает за взаимодейстие с попапом-подвержденим действия (без формы)
+   * @param {string} popupSelector - Селектор элемента попапа
+   * @param {function} handleSubmit - Колбек для обработки нажатия на кнопку
+   */
   constructor(popupSelector, handleSubmit) {
     super(popupSelector);
     this._handleSubmit = handleSubmit;
     this._button = this._popup.querySelector('.popup__save-button');
   }
 
+  /**
+   * Устанавливает необходимые слушатели событий на элементы попапа
+   */
   setEventListeners() {
     super.setEventListeners();
 
@@ -15,6 +23,10 @@ export default class PopupWithConfirmation extends Popup {
     });
   }
 
+  /**
+   * Задает целевой параметр, с которым работает попап и котрое возвращает колбеку
+   * @param {*} target - Целевой параметр
+   */
   setTarget(target) {
     this._target = target;
   }
