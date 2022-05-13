5a8519603ac123bf8a4b13c02c1b23fc7d184ef4
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-05-13 14:31:20 +0300

    refactor: добавить комментарии в Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,12 @@
 export default class Section {
   /**
-   * Класс отвечает за отрисовку элементов на странице
+   * Отвечает за вывод элементов на страницу в определенном блоке
+   * @constructor
    *
-   * Параметры:
-   * items - массив данных, которые нужно добавить на страницу при инициализации класса,
-   * renderer - функция, которая отвечает за создание и отрисовку данных на странице,
-   * containerSelector - селектор контейнера, в который нужно добавлять созданные элементы
+   * @param {object} Объект с параметрами:
+   * - items - Массив данных, которые нужно добавить на страницу при инициализации класса,
+   * - renderer - Функция, которая отвечает за создание и отрисовку данных на странице,
+   * @param {string} containerSelector - Селектор контейнера, в который нужно добавлять созданные элементы
    */
   constructor({items, renderer}, containerSelector) {
     this._items = items;
@@ -13,6 +14,9 @@ export default class Section {
     this._container = document.querySelector(containerSelector);
   }
 
+  /**
+   * Добавляет все исходные элементы на страницу
+   */
   renderItems() {
     this._items.forEach(item => {
       const element = this._renderer(item);
@@ -20,6 +24,11 @@ export default class Section {
     })
   }
 
+  /**
+   * Добвляет элемент на страницу
+   * @param {object} item - Элемент для добавления
+   * @param {boolean} isInversed - Определяет порядок вставки, по умолчанию - в конец контейнера
+   */
   addItem(item, isInversed = false) {
     if (isInversed) {
       this._container.prepend(item);
