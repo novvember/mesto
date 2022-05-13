ab631ad9b3f6cc75035ef44c31132f5dbb94dbf9
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-05-13 14:12:18 +0300

    refactor: добавить комментарии в Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,32 +1,43 @@
 export default class Popup {
   /**
-   * Класс отвечает за открытие и закрытие попапа
-   *
-   * Параметры:
-   * popupSelector - селектор попапа
-   *
+   * Отвечает за открытие и закрытие попапа
+   * @constructor
+   * @param {string} popupSelector - Селектор элемента попапа
    */
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
+  /**
+   * Открывает попап
+   */
   open() {
     this._popup.classList.add('popup_opened');
     document.addEventListener('keydown', this._handleEscClose);
   }
 
+  /**
+   * Закрывает попап
+   */
   close() {
     this._popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
   }
 
+  /**
+   * Обрабатывает нажатия на Escape для закрытия попапа
+   * @param {object} evt - Событие, получаемое из слушателя
+   */
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
       this.close();
     }
   }
 
+  /**
+   * Устанавливает слушатели событий на элементы попапа для управления его закрытием
+   */
   setEventListeners() {
     this._popup.addEventListener('mousedown', (evt) => {
       if (evt.target === evt.currentTarget) this.close();
